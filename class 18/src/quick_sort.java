0f7c783ef2db7adc65de8e9ea1ce6066296e01c6
MananBVP <manan200998@gmail.com>
2018-10-17 19:35:10 +0530

    get polynomial value

--- class 18/src/quick_sort.java
+++ class 18/src/quick_sort.java
@@ -13,11 +13,12 @@ public class quick_sort {
 //		for (int val : arr) {
 //			System.out.print(val + " ");
 //		}
-		int[] arr = { 8, 14, 19, 11, 7, 22, 11 };
-		quicksort(arr, 0, arr.length-1);
-		for (int val : arr) {
-			System.out.print(val + " ");
-		}
+//		int[] arr = { 8, 14, 19, 11, 7, 22, 11 };
+//		quicksort(arr, 0, arr.length-1);
+//		for (int val : arr) {
+//			System.out.print(val + " ");
+//		}
+		System.out.println(getPolyVal(2,3));
 	}
 
 	private static int partitioning(int[] arr, int pivot) {
@@ -43,26 +44,27 @@ public class quick_sort {
 	}
 
 	private static void sort0_1_2(int[] arr) {
-		int i = 0; //just after the last 0 i.e. 1st 1 
-		int j = 0; //just after the last 1 i.e. 1st 2
-		int k = 0; //just after the last 2 i.e. 1st unknown
+		int i = 0; // just after the last 0 i.e. 1st 1
+		int j = 0; // just after the last 1 i.e. 1st 2
+		int k = 0; // just after the last 2 i.e. 1st unknown
 		while (k < arr.length) {
 			if (arr[k] == 2) {
-				k++; //2's area grows
+				k++; // 2's area grows
 			} else if (arr[k] == 1) {
 				swap(arr, j, k);
-				j++;//1's area grows
-				k++;//2's area grows
+				j++;// 1's area grows
+				k++;// 2's area grows
 			} else if (arr[k] == 0) {
 				swap(arr, j, k);
-				k++;//2's area grows
+				k++;// 2's area grows
 				swap(arr, i, j);
-				i++;//0's area grows
-				j++;//1's area grows
+				i++;// 0's area grows
+				j++;// 1's area grows
 			}
 		}
 	}
-	private static void quicksort(int[] arr,int lo,int hi) {
+
+	private static void quicksort(int[] arr, int lo, int hi) {
 		if (lo >= hi) {
 			return;
 		}
@@ -70,21 +72,41 @@ public class quick_sort {
 		quicksort(arr, lo, pivot - 1);
 		quicksort(arr, pivot + 1, hi);
 	}
-	private static int partitioningqs(int[] arr,int lo,int hi) {
-		int pivot=arr[hi];
-		int i=lo;
-		int p=lo;
-		while(i<=hi) {
-			if(arr[i]>pivot) {
+
+	private static int partitioningqs(int[] arr, int lo, int hi) {
+		int pivot = arr[hi];
+		int i = lo;
+		int p = lo;
+		while (i <= hi) {
+			if (arr[i] > pivot) {
 				i++;
-			}
-			else {
+			} else {
 				swap(arr, i, p);
 				i++;
 				p++;
 			}
 		}
-		return p-1;
+		return p - 1;
+	}
 
+//	private static int getPolyVal(int x, int n) {
+//		int sum = 0;
+//		int xpow = x;
+//		for (int i = n; i >= 1; i--) {
+//			sum += i * xpow;
+//			xpow*=x;
+//		}
+//		return sum;
+//	}
+	private static int getPolyVal(int x, int n) {
+		int sum = 0;
+		int co=n;
+		int xpow = x;
+		for (int i = 1; i <= n; i++) {
+			sum += co * xpow;
+			co--;
+			xpow*=x;
+		}
+		return sum;
 	}
 }
