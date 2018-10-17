b646902cd448a5bc9e1615e53468f4f9c650d905
MananBVP <manan200998@gmail.com>
2018-10-17 19:15:39 +0530

    quick sort

--- class 18/src/quick_sort.java
+++ class 18/src/quick_sort.java
@@ -2,14 +2,19 @@ public class quick_sort {
 
 	public static void main(String[] args) {
 //		int[] arr = { 8, 14, 19, 11, 7, 22, 11 };
-		int[] arr = {0,0,2,2,0,1,1,2,0,1,0};
+//		int[] arr = { 0, 0, 2, 2, 0, 1, 1, 2, 0, 1, 0 };
 //		int a = partitioning(arr, 11);
 //		for (int val : arr) {
 //			System.out.print(val + " ");
 //		}
 //		System.out.println();
 //		System.out.println(a);
-		sort0_1_2(arr);
+//		sort0_1_2(arr);
+//		for (int val : arr) {
+//			System.out.print(val + " ");
+//		}
+		int[] arr = { 8, 14, 19, 11, 7, 22, 11 };
+		quicksort(arr, 0, arr.length-1);
 		for (int val : arr) {
 			System.out.print(val + " ");
 		}
@@ -38,26 +43,48 @@ public class quick_sort {
 	}
 
 	private static void sort0_1_2(int[] arr) {
-		int i = 0;
-		int j = 0;
-		int k = 0;
+		int i = 0; //just after the last 0 i.e. 1st 1 
+		int j = 0; //just after the last 1 i.e. 1st 2
+		int k = 0; //just after the last 2 i.e. 1st unknown
 		while (k < arr.length) {
 			if (arr[k] == 2) {
-				k++;
+				k++; //2's area grows
 			} else if (arr[k] == 1) {
 				swap(arr, j, k);
-				j++;
-				k++;
-			}
-			else if(arr[k]==0) {
+				j++;//1's area grows
+				k++;//2's area grows
+			} else if (arr[k] == 0) {
 				swap(arr, j, k);
-				k++;
+				k++;//2's area grows
 				swap(arr, i, j);
-				i++;
-				j++;
+				i++;//0's area grows
+				j++;//1's area grows
 			}
 		}
+	}
+	private static void quicksort(int[] arr,int lo,int hi) {
+		if (lo >= hi) {
+			return;
+		}
+		int pivot = partitioningqs(arr, lo, hi);
+		quicksort(arr, lo, pivot - 1);
+		quicksort(arr, pivot + 1, hi);
+	}
+	private static int partitioningqs(int[] arr,int lo,int hi) {
+		int pivot=arr[hi];
+		int i=lo;
+		int p=lo;
+		while(i<=hi) {
+			if(arr[i]>pivot) {
+				i++;
+			}
+			else {
+				swap(arr, i, p);
+				i++;
+				p++;
+			}
+		}
+		return p-1;
 
 	}
-
 }
