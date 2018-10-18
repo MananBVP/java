b19675da0e3b397e2a702ed381b75374386c7931
MananBVP <manan200998@gmail.com>
2018-10-18 19:18:38 +0530

    build and query

--- class 22/src/range_query.java
+++ class 22/src/range_query.java
@@ -1,7 +1,55 @@
-public class egg_drop {
+public class range_query {
+	static int[] sa;
+	static int[] ba;
 
 	public static void main(String[] args) {
+		ba=new int[] {8,19,2,-6,5,12};
+		int h=getLog(ba.length, false);
+		int sz=(1<<(h+1)) -1;
+		sa=new int[sz];
+		build(0,0,ba.length-1);
 		
+		System.out.println(query(0, 0, ba.length-1, 1, 3));
+		System.out.println(query(0, 0, ba.length-1, 1, 3));
 	}
+	
+	private static void build(int si,int ss,int se) {
+		if(ss==se) {
+			sa[si]=ba[ss];
+			return;
+		}
+		int mid=(ss+se)/2;
+		build(2*si+1, ss, mid);
+		build(2*si+2, mid+1, se);
+		
+		sa[si]=sa[2*si+1]+sa[2*si+2];
+	}
+	
+	private static int query(int si,int ss,int se,int qs,int qe) {
+		if(ss>=qs && se<=qe) { //segment starts before query starts and segment ends before query ends
+			return sa[si];
+		}else if(ss>qe || se<qs) { //segment ends before the query starts and segment starts after the query ends
+			return 0;
+		}else {
+			int mid=(ss+se)/2;
+			int lc=query(2*si+1, ss, mid, qs, qe);
+			int rc=query(2*si+2, mid+1, se, qs, qe);
+			return lc+rc;
+		}
+	}
+	
+	private static void update(int si,int ss,int se,int bi,int delta) {
 
+	}
+	
+	private static int getLog(int n,boolean floor) {
+		int log=0;
+		while((1<<log) < n) {
+			log++;
+		}
+		if(floor && (1<<log) < n)
+			log--;
+		
+		return log;
+	}
 }
