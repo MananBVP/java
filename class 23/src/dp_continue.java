b29569d86d12d99d8be81252708d7c8f779991f6
MananBVP <manan200998@gmail.com>
2018-10-20 18:54:19 +0530

    0-1 knapsack

--- class 23/src/dp_continue.java
+++ class 23/src/dp_continue.java
@@ -1,11 +1,14 @@
 public class dp_continue {
 
 	public static void main(String[] args) {
-		int[] arr = { 5, 2, 4, 1, 3 };
-		int sum = 0;
+//		int[] arr = { 5, 2, 4, 1, 3 };
+//		int sum = 0;
 //		boolean[][] strg = new boolean[arr.length + 1][sum + 1];
-		boolean[][] strg = printSubarraySum(arr, 10);
-		printSS(arr, strg, "", strg.length - 1, strg[0].length - 1);
+//		boolean[][] strg = printSubarraySum(arr, 10);
+//		printSS(arr, strg, "", strg.length - 1, strg[0].length - 1);
+		int[] wt = { 2, 5, 1, 3, 4 };
+		int[] price = { 15, 14, 10, 16, 30 };
+		knapsack(wt, price, 7);
 	}
 
 	private static boolean[][] printSubarraySum(int[] arr, int sum) {
@@ -42,7 +45,19 @@ public class dp_continue {
 			printSS(arr, strg, asf + " " + arr[i - 1], i - 1, j - arr[i - 1]);
 		}
 	}
-	private static void knapsack() {
-		hgh
+
+	private static void knapsack(int[] wt, int[] price, int capacity) {
+		int[][] strg = new int[wt.length + 1][capacity + 1];
+		
+		for (int i = 1; i < strg.length; i++) {
+			for (int j = 1; j < strg[0].length; j++) {
+				strg[i][j]=strg[i-1][j];
+				if (j-wt[i-1]>=0) {
+					strg[i][j]=Math.max(strg[i - 1][j],strg[i - 1][j - wt[i - 1]]+ price[i - 1]);
+				}
+			}
+		}
+		System.out.println(strg[strg.length-1][strg[0].length-1]);
 	}
+	
 }
