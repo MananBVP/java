6bd1412be9b9fd06ac2fb88dbc39f44fb23cbc6b
MananBVP <manan200998@gmail.com>
2018-10-21 18:01:28 +0530

    Coin Change Per

--- class 24/src/dp_continue.java
+++ class 24/src/dp_continue.java
@@ -1,8 +1,11 @@
 public class dp_continue {
 
 	public static void main(String[] args) {
-		int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60, 80, 1 };
-		LongestBitonicSS(arr);
+//		int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60, 80, 1 };
+//		LongestBitonicSS(arr);
+		int[] coins = { 2, 3, 5, 6 };
+		CoinChangePTab(coins, 10);
+		CoinChangeCTab(coins, 10);
 
 	}
 
@@ -33,8 +36,30 @@ public class dp_continue {
 		}
 		System.out.println(omax);
 	}
-	private static void CoinChange() {
-		// TODO Auto-generated method stub
 
+	private static void CoinChangePTab(int[] coins, int t) {
+		int[] strg = new int[t + 1];
+		strg[0] = 1;
+		for (int i = 1; i < strg.length; i++) {
+			for (int j = 0; j < coins.length; j++) {
+				if (i - coins[j] >= 0) {
+					strg[i] += strg[i - coins[j]];
+				}
+			}
+		}
+		System.out.println(strg[strg.length-1]);
+	}
+
+	private static void CoinChangeCTab(int[] coins, int t) {
+		int[] strg = new int[t + 1];
+		strg[0] = 1;
+		for (int j = 0; j < coins.length; j++) {
+			for (int i = 1; i < strg.length; i++) {
+				if (i - coins[j] >= 0) {
+					strg[i] += strg[i - coins[j]];
+				}
+			}
+		}
+		System.out.println(strg[strg.length-1]);
 	}
 }
