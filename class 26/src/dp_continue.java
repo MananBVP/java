52fecae3de288579a76a784d17554ef54a0f0d55
MananBVP <manan200998@gmail.com>
2018-10-27 19:26:24 +0530

    edit distance Tab

--- class 26/src/dp_continue.java
+++ class 26/src/dp_continue.java
@@ -7,10 +7,11 @@ public class dp_continue {
 //		System.out.println(lcs(s1, s2));
 //		System.out.println(lcsM(s1, s2, new Integer[s1.length() + 1][s2.length() + 1]));
 //		System.out.println(lcsT(s1, s2));
-		String s1 = "abcdeghi";
-		String s2 = "acdfgkhi";
+		String s1 = "abcdegghjthduuwghhi";
+		String s2 = "acdfgkfejhi";
 //		System.out.println(editDistance(s1, s2));
 		System.out.println(editDistanceM(s1, s2, new Integer[s1.length() + 1][s2.length() + 1]));
+		System.out.println(editDistanceT(s1, s2));
 	}
 
 	private static int lcs(String s1, String s2) {
@@ -91,7 +92,8 @@ public class dp_continue {
 		}
 		return ans;
 	}
-	private static int editDistanceM(String s1, String s2,Integer[][] qb) {
+
+	private static int editDistanceM(String s1, String s2, Integer[][] qb) {
 		if (s1.length() == 0) {
 			return s2.length();// delete
 		}
@@ -109,12 +111,57 @@ public class dp_continue {
 		if (ch1 == ch2) {
 			return editDistance(ros1, ros2);
 		} else {
-			int finsert = editDistanceM(ros1, s2,qb) + 1;
-			int fdelete = editDistanceM(s1, ros2,qb) + 1;
-			int freplace = editDistanceM(ros1, ros2,qb) + 1;
+			int finsert = editDistanceM(ros1, s2, qb) + 1;
+			int fdelete = editDistanceM(s1, ros2, qb) + 1;
+			int freplace = editDistanceM(ros1, ros2, qb) + 1;
 			ans = Math.min(finsert, Math.min(fdelete, freplace));
 		}
 		qb[s1.length()][s2.length()] = ans;
 		return ans;
+	}
+
+//	private static int editDistanceT(String s1, String s2) {
+//		int[][] strg = new int[s1.length() + 1][s2.length() + 1];
+//		for (int i = strg.length - 1; i >= 0; i--) {
+//			for (int j = strg[0].length - 1; j >= 0; j--) {
+//				if (i == strg.length - 1 && j == strg[0].length - 1) {
+//					strg[i][j] = 0;
+//				} else if (i == strg.length - 1) {
+//					strg[i][j] = strg[0].length-j;
+//				} else if (j == strg[0].length - 1) {
+//					strg[i][j] = strg.length-i;
+//				} else {
+//					if (s1.charAt(i) == s2.charAt(j)) {
+//						strg[i][j] = strg[i + 1][j + 1];
+//					} else {
+//						strg[i][j] = Math.min(strg[i + 1][j], Math.min(strg[i][j + 1], strg[i + 1][j + 1])) + 1;
+//					}
+//				}
+//			}
+//		}
+//		return strg[0][0];
+//	}
+	private static int editDistanceT(String s1, String s2) {
+		int[][] strg = new int[s1.length() + 1][s2.length() + 1];
+		int counter = 0;
+		for (int i = strg[0].length - 1; i >= 0; i--) {
+			strg[strg.length - 1][i] = counter;
+			counter++;
+		}
+		counter = 0;
+		for (int i = strg.length - 1; i >= 0; i--) {
+			strg[i][strg[0].length - 1] = counter;
+			counter++;
+		}
+		for (int i = strg.length - 2; i >= 0; i--) {
+			for (int j = strg[0].length - 2; j >= 0; j--) {
+				if (s1.charAt(i) == s2.charAt(j)) {
+					strg[i][j] = strg[i + 1][j + 1];
+				} else {
+					strg[i][j] = Math.min(strg[i + 1][j], Math.min(strg[i][j + 1], strg[i + 1][j + 1])) + 1;
+				}
+			}
+		}
+		return strg[0][0];
 	}
 }
