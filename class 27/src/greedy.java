0746bfb0724005f115398ef19cc34cb52e794233
MananBVP <manan200998@gmail.com>
2018-10-28 16:40:01 +0530

    greedy

---
+++ class 27/src/greedy.java
@@ -0,0 +1,15 @@
+
+public class greedy {
+
+	public static void main(String[] args) {
+		egyption_Fraction(3, 7);
+	}
+	private static void egyption_Fraction(int num,int denom) {
+		int x,y;
+		x=num;
+		y=denom;
+		if(x==1) {
+			System.out.println(x+"-"+y);
+		}
+	}
+}
