9c058b93e712c11093b9758b8a574afc07a45c97
MananBVP <manan200998@gmail.com>
2018-10-28 17:43:01 +0530

    maxSumNoAdjacent

--- class 27/src/greedy.java
+++ class 27/src/greedy.java
@@ -2,7 +2,9 @@
 public class greedy {
 
 	public static void main(String[] args) {
-		egyption_Fraction(3, 7);
+//		egyption_Fraction(3, 7);
+		int[] arr= {5,6,10,100,10,5};
+		maxSumNoAdjacent(arr);
 	}
 
 	private static void egyption_Fraction(int num, int denom) {
@@ -17,5 +19,15 @@ public class greedy {
 		int newnum=1;
 		System.out.println(1+"/"+newdenom);
 		egyption_Fraction(num*newdenom-denom, newdenom*denom);
+	}
+	private static void maxSumNoAdjacent(int[] arr) {
+		int inc=arr[0];
+		int exc=0;
+		for(int i=1;i<arr.length;i++) {
+			int oinc=inc;
+			inc=exc+arr[i];
+			exc=Math.max(oinc, exc);
+		}
+	System.out.println(Math.max(inc, exc));
 	}
 }
