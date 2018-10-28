1296f5a8037f05ffbf96fae1ef95af9dce371f07
MananBVP <manan200998@gmail.com>
2018-10-28 16:58:22 +0530

    egyptian fraction

--- class 27/src/greedy.java
+++ class 27/src/greedy.java
@@ -4,12 +4,18 @@ public class greedy {
 	public static void main(String[] args) {
 		egyption_Fraction(3, 7);
 	}
-	private static void egyption_Fraction(int num,int denom) {
-		int x,y;
-		x=num;
-		y=denom;
-		if(x==1) {
-			System.out.println(x+"-"+y);
+
+	private static void egyption_Fraction(int num, int denom) {
+		if(num==0) {
+			return;
 		}
+		if (num == 1) {
+			System.out.println(num+"/"+denom);
+			return;
+		}
+		int newdenom=(int)Math.ceil(denom*1.0/num);
+		int newnum=1;
+		System.out.println(1+"/"+newdenom);
+		egyption_Fraction(num*newdenom-denom, newdenom*denom);
 	}
 }
