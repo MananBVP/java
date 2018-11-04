dbda5f3310655227b22d750dd655163a8c48aaf1
MananBVP <manan200998@gmail.com>
2018-11-04 18:34:26 +0530

    largest_histogram-area

--- class 29/src/stock_span.java
+++ class 29/src/stock_span.java
@@ -2,12 +2,14 @@
 public class stock_span {
 
 	public static void main(String[] args) {
-		int[] prices = { 30, 40, 50, 45, 48, 60, 20, 25, 38, 32, 37, 50 };
-		int[] spans = Stock_spn(prices);
-		for (int val : spans) {
-			System.out.print(val + " ");
-		}
-		System.out.println();
+//		int[] prices = { 30, 40, 50, 45, 48, 60, 20, 25, 38, 32, 37, 50 };
+//		int[] spans = Stock_spn(prices);
+//		for (int val : spans) {
+//			System.out.print(val + " ");
+//		}
+//		System.out.println();
+		int[] arr = { 6, 2, 5, 4, 5, 1, 6 };
+		System.out.println(Largest_Histogram_Area(arr));
 	}
 
 	private static int[] Stock_spn(int[] prices) {
@@ -24,7 +26,38 @@ public class stock_span {
 		}
 		return spans;
 	}
-	private static void Largest_Histogram_Area() {
-		
+
+	private static int Largest_Histogram_Area(int[] arr) {
+		stack st = new stack(arr.length);
+		int maxa = Integer.MIN_VALUE;
+		st.push(0);
+		int carea = 0;
+		for (int i = 1; i < arr.length; i++) {
+			while (st.size() > 0 && arr[i] < arr[st.top()]) {
+				int breadth = 0;
+				int val = arr[st.pop()];
+				if (st.size() == 0) {
+					breadth = i;
+				} else {
+					breadth = i - st.top() - 1;
+				}
+				carea = val * breadth;
+				maxa = Math.max(carea, maxa);
+			}
+			st.push(i);
+		}
+		while (st.size() > 0) {
+			int breadth = 0;
+
+			int val = arr[st.pop()];
+			if (st.size() == 0) {
+				breadth = arr.length;
+			} else {
+				breadth = arr.length - st.top() - 1;
+			}
+			carea = val * breadth;
+			maxa = Math.max(carea, maxa);
+		}
+		return maxa;
 	}
 }
