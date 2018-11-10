4809e7228a585b580b8f65b68665dca52c138cb7
MananBVP <manan200998@gmail.com>
2018-11-10 17:47:39 +0530

    Two Stacks

--- class 30/src/Client_stack.java
+++ class 30/src/Client_stack.java
@@ -1,5 +1,5 @@
 
-public class Client {
+public class Client_stack {
 
 	public static void main(String[] args) {
 		dynamic_stack obj=new dynamic_stack(5);
@@ -13,6 +13,20 @@ public class Client {
 		obj.push(80);
 		obj.push(90);
 		obj.display();
+		twostack();
 	}
-
+	public static void twostack() {
+		TwoStacks ts=new TwoStacks(5);
+		ts.push1(10);
+		ts.push1(20);
+		ts.push1(30);
+		ts.push2(40);
+		ts.push2(50);
+		ts.push2(60);
+		System.out.println(ts.pop1());
+		System.out.println(ts.pop2());
+		
+		System.out.println(ts.top1());
+		System.out.println(ts.top2());
+	}
 }
---
+++ class 30/src/TwoStacks.java
@@ -0,0 +1,70 @@
+
+public class TwoStacks {
+	int[] arr;
+	int tos1;
+	int tos2;
+
+	public TwoStacks(int cap) {
+		arr = new int[cap];
+		tos1 = -1;
+		tos2 = arr.length;
+	}
+
+	public void push1(int val) {
+		if(tos1+1==tos2) {
+			System.out.println("Stack 1 is full");
+			return;
+		}
+		tos1++;
+		arr[tos1]=val;
+	}
+
+	public int pop1() {
+		if(tos1==-1) {
+			System.out.println("Stack 1 is empty");
+			return -1;
+		}
+		int rv=arr[tos1];
+		arr[tos1]=0;
+		tos1--;
+		return rv;
+	}
+
+	public int top1() {
+		if(tos1==-1) {
+			System.out.println("Stack 1 is empty");
+			return -1;
+		}
+		int rv=arr[tos1];
+		return rv;
+	}
+
+	public void push2(int val) {
+		if(tos2-1==tos1) {
+			System.out.println("Stack 2 is full");
+			return;
+		}
+		tos2--;
+		arr[tos2]=val;
+	}
+
+	public int pop2() {
+		if(tos2==arr.length) {
+			System.out.println("Stack 2 is empty");
+			return -1;
+		}
+		int rv=arr[tos2];
+		arr[tos2]=0;
+		tos2++;
+		return rv;
+	}
+
+	public int top2() {
+		if(tos2==arr.length) {
+			System.out.println("Stack 2 is empty");
+			return -1;
+		}
+		int rv=arr[tos2];
+		return rv;
+	}
+}
