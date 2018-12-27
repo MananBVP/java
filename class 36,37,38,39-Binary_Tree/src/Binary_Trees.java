2b6e127192e8db5792dd00230c2829289847f3be
MananBVP <manan200998@gmail.com>
2018-12-27 18:27:25 +0530

    largest bst in left or right

--- class 36,37,38,39-Binary_Tree/src/Binary_Trees.java
+++ class 36,37,38,39-Binary_Tree/src/Binary_Trees.java
@@ -505,20 +505,6 @@ public class Binary_Trees {
 			mp.min = Math.min(node.data , Math.min(lp.min, rp.min));
 			mp.isBST = lp.isBST && rp.isBST && node.data > lp.max && node.data < rp.min;
 			
-//			if(lp.isBST == true && rp.isBST == false) {
-//				mp.lbroot = lp.lbroot;
-//				mp.lbsize = lp.lbsize;
-//			}else if(rp.isBST == true && lp.isBST == false) {
-//				mp.lbroot = rp.lbroot;
-//				mp.lbsize = rp.lbsize;
-//			}else if(rp.isBST == false && lp.isBST == false) {
-//				mp.lbsize = lp.lbsize > rp.lbsize ? lp.lbsize : rp.lbsize;
-//				mp.lbroot = lp.lbsize > rp.lbsize ? lp.lbroot : rp.lbroot;
-//			}else{
-//				mp.lbsize = lp.lbsize + rp.lbsize + 1;
-//				mp.lbroot = node;
-//			}
-			
 			if(mp.isBST == true) {
 				mp.lbroot = node;
 				mp.lbsize = lp.lbsize + rp.lbsize + 1;
