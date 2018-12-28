a4ccaf0a88c10a86c89653d8107de3f2feb7f720
MananBVP <manan200998@gmail.com>
2018-12-28 17:26:58 +0530

    replaceWithSumOfLargerNodes

--- class 40/src/BST.java
+++ class 40/src/BST.java
@@ -9,18 +9,17 @@ public class BST {
 
 	private Node root;
 	private int size;
-	
+
 	public BST(int[] sa) {
 		root = construct(sa, 0, sa.length - 1);
 	}
-	
-	private Node construct(int[] sa , int lo , int hi) {
+
+	private Node construct(int[] sa, int lo, int hi) {
 		Node node = new Node();
-		if(lo > hi) {
+		if (lo > hi) {
 			return null;
 		}
-		
-		
+
 		int mid = (lo + hi) / 2;
 		node.data = sa[mid];
 		node.left = construct(sa, lo, mid - 1);
@@ -28,7 +27,7 @@ public class BST {
 		size++;
 		return node;
 	}
-	
+
 	public void display() {
 		display(root);
 	}
@@ -46,70 +45,91 @@ public class BST {
 		display(node.left);
 		display(node.right);
 	}
-	
+
 	public int max() {
 		return max(root);
 	}
-	
+
 	private int max(Node node) {
-		if(node.right == null) {
+		if (node.right == null) {
 			return node.data;
 		}
-		
+
 		return max(node.right);
 	}
-	
+
 	public int min() {
 		return min(root);
 	}
-	
+
 	private int min(Node node) {
-		if(node.left == null) {
+		if (node.left == null) {
 			return node.data;
 		}
-		
+
 		return min(node.left);
 	}
-	
+
 	public boolean find(int data) {
 		return find(root, data);
 	}
-	
-	private boolean find(Node node , int data) {
-		if(node == null) {
+
+	private boolean find(Node node, int data) {
+		if (node == null) {
 			return false;
 		}
-		
-		if(node.data == data) {
+
+		if (node.data == data) {
 			return true;
 		}
-		
+
 //		return node.data > data ? find(node.left , data) : find(node.right, data);
-		if(node.data > data) {
+		if (node.data > data) {
 			return find(node.left, data);
-		}else {
+		} else {
 			return find(node.right, data);
-			
+
 		}
 	}
-	
-	public void printInRange(int lo , int hi) {
+
+	public void printInRange(int lo, int hi) {
 		printInRange(root, lo, hi);
 	}
-	
-	private void printInRange(Node node , int lo , int hi) {
-		if(node == null) {
+
+	private void printInRange(Node node, int lo, int hi) {
+		if (node == null) {
 			return;
 		}
 		
-		if(node.data >= lo && node.data <= hi) {
+		if (node.data >= lo && node.data <= hi) {
 			printInRange(node.left, lo, hi);
 			System.out.print(node.data + " ");
 			printInRange(node.right, lo, hi);
-		}else if(node.data < lo) {
+		} else if (node.data < lo) {
 			printInRange(node.right, lo, hi);
-		}else if(node.data > hi) {
+		} else if (node.data > hi) {
 			printInRange(node.left, lo, hi);
 		}
+	}
+
+	private int rsum;
+	
+	public void replaceWithSumOfLargerNodes() {
+		rsum = 0;
+		replaceWithSumOfLargerNodes(root);
+	}
+
+	private void replaceWithSumOfLargerNodes(Node node) {
+		if (node == null) {
+			return;
+		}
+		
+		replaceWithSumOfLargerNodes(node.right);
+		
+		int ond = node.data;
+		node.data = rsum;
+		rsum += ond;
+		
+		replaceWithSumOfLargerNodes(node.left);
 	}
 }
--- class 40/src/Client.java
+++ class 40/src/Client.java
@@ -11,8 +11,15 @@ public class Client {
 //		System.out.println(bt.find(11));
 		int[] sa1= {12 , 25 , 30 , 37 , 40 , 50 , 60 , 62 , 70 ,  75 , 87};
 		BST bt1 = new BST(sa1);
-		bt1.printInRange(25, 75);
-
+//		bt1.display();
+		System.out.println("---------------");
+//		bt1.printInRange(26, 80);
+		int[] sa2= {10 , 20 , 30 , 50 , 60 , 70 , 80};
+		BST bst2 = new BST(sa2);
+		bst2.display();
+		System.out.println("---------------");
+		bst2.replaceWithSumOfLargerNodes();
+		bst2.display();
 	}
 
 }
