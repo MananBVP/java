7e8bd2157dedfa64c5728b9b685a549162593135
MananBVP <manan200998@gmail.com>
2018-12-31 08:50:17 +0530

    printKLargest

--- class 42/src/PQClient.java
+++ class 42/src/PQClient.java
@@ -1,9 +1,28 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class PQClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = { 2, 3, 6, 8, 9, 7, 5 };
+		printKLargest(arr, 3);
 
 	}
 
+	public static void printKLargest(int[] arr, int k) { // space = O(n) time = O(n + k)logn
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+		for (int val : arr) {
+			pq.add(val);
+		}
+
+		for (int i = 0; i < k; i++) {
+			System.out.print(pq.remove() + " ");
+		}
+	}
+
+	public static void printKLargestBetter(int[] arr, int k) { // space = O(k) time = O(nlogk)
+
+	}
 }
