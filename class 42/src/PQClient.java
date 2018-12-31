262429d29bda89e391ab217c1a05ae7c47b252a2
MananBVP <manan200998@gmail.com>
2018-12-31 09:08:48 +0530

    printKLargestBetter

--- class 42/src/PQClient.java
+++ class 42/src/PQClient.java
@@ -4,8 +4,9 @@ import java.util.PriorityQueue;
 public class PQClient {
 
 	public static void main(String[] args) {
-		int[] arr = { 2, 3, 6, 8, 9, 7, 5 };
+		int[] arr = { 2, 3, 6, 8, 9, 7, 5, 11, 17, 1 };
 		printKLargest(arr, 3);
+		printKLargestBetter(arr, 3);
 
 	}
 
@@ -23,6 +24,19 @@ public class PQClient {
 	}
 
 	public static void printKLargestBetter(int[] arr, int k) { // space = O(k) time = O(nlogk)
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 
+		pq.add(arr[0]);
+
+		for (int i = 0; i < arr.length; i++) {
+			if (i < k) {
+				pq.add(arr[i]);
+			} else if (arr[i] > pq.peek()) {
+				pq.remove();
+				pq.add(arr[i]);
+			}
+		}
+
+		System.out.print(pq);
 	}
 }
