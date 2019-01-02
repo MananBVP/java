d7e6e39dc0e481f6077d7e69c0da0ac2509606fb
MananBVP <manan200998@gmail.com>
2019-01-02 19:16:04 +0530

    Hashmap Implementaion

--- class 44 Hashmap_Implementation/src/Client.java
+++ class 44 Hashmap_Implementation/src/Client.java
@@ -2,11 +2,41 @@
 public class Client {
 
 	public static void main(String[] args) {
-		Linked_List<String> ls = new Linked_List<>();
-		ls.addLast("hello");
-		ls.addLast("world");
-		ls.addLast("Java");
-		ls.dispaly();
+//		Linked_List<String> ls = new Linked_List<>();
+//		ls.addLast("hello");
+//		ls.addLast("world");
+//		ls.addLast("Java");
+//		ls.dispaly();
+		
+		Hash_Map<String, Integer> pmap = new Hash_Map<>();
+		pmap.put("I", 120);
+		pmap.put("C", 200);
+		pmap.put("P", 80);
+		pmap.put("US", 20);
+		pmap.put("UK", 50);
+		pmap.put("Aus",25);
+		pmap.put("NZ", 10);
+		pmap.put("Can", 5);
+		pmap.display();
+		
+		pmap.put("Aus",30);
+		pmap.display();
+		
+		System.out.println(pmap.get("US"));
+		System.out.println(pmap.get("Sw"));
+		System.out.println(pmap.containsKey("US"));
+		System.out.println(pmap.containsKey("Sw"));
+		
+		System.out.println(pmap.keySet());
+		System.out.println(pmap.remove("P"));
+		System.out.println(pmap.remove("Sw"));
+		pmap.display();
+		System.out.println(pmap.keySet());
+		
+		pmap.put("P", 80);
+		pmap.put("Sl", 40);
+		pmap.display();
+
 
 	}
 
--- class 44 Hashmap_Implementation/src/Hash_Map.java
+++ class 44 Hashmap_Implementation/src/Hash_Map.java
@@ -69,22 +69,45 @@ public class Hash_Map<K, V> {
 		} else {
 			hmnode.value = value;
 		}
+
+		double lambda = size * 1.0 / buckets.length;
+		if (lambda > 2.0) {
+			rehash();
+		}
 	}
 
 	public ArrayList<K> keySet() {
+		ArrayList<K> list = new ArrayList<>();
+		for (int bi = 0; bi < buckets.length; bi++) {
+			for (int di = 0; di < buckets[bi].size; di++) {
+				HMNode hmnode = buckets[bi].getAt(di);
+				list.add(hmnode.key);
+			}
+		}
 
+		return list;
 	}
 
 	public int size() {
-
+		return size;
 	}
 
 	public boolean isEmpty() {
-
+		return size == 0;
 	}
 
 	public void display() {
-
+		System.out.println("------------------");
+		for (int bi = 0; bi < buckets.length; bi++) {
+			System.out.print("B" + bi + " ");
+			for (int di = 0; di < buckets[bi].size; di++) {
+				HMNode hmnode = buckets[bi].getAt(di);
+				System.out.print("[" + hmnode.key + "_" + hmnode.value + "]");
+				
+			}
+			System.out.println(".");
+		}
+		System.out.println("-------------------");
 	}
 
 	private int hashFunction(K key) {
@@ -108,8 +131,27 @@ public class Hash_Map<K, V> {
 		for (int di = 0; di < buckets[bi].size; di++) {
 			HMNode hmnode = buckets[bi].getAt(di);
 			if (hmnode.key.equals(key)) {
-				buckets[bi].removeAt(di) ;
+				buckets[bi].removeAt(di);
 				return;
+			}
+		}
+	}
+
+	private void rehash() {
+		Linked_List<HMNode>[] oba = buckets;
+
+		buckets = new Linked_List[2 * buckets.length];
+		for (int i = 0; i < buckets.length; i++) {
+			buckets[i] = new Linked_List<>();
+		}
+
+		
+		HMNode hmnode;
+		size = 0;
+		for (int bi = 0; bi < oba.length; bi++) {
+			for (int di = 0; di < oba[bi].size; di++) {
+				hmnode = oba[bi].getAt(di);
+				put(hmnode.key, hmnode.value);
 			}
 		}
 	}
