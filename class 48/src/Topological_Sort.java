ada71c2cea4f51b085c94242f51fe980c722d0d8
MananBVP <manan200998@gmail.com>
2019-01-12 17:31:24 +0530

    topological sort

--- class 48/src/Topological_Sort.java
+++ class 48/src/Topological_Sort.java
@@ -5,25 +5,30 @@ public class Topological_Sort {
 
 	public static void main(String[] args) {
 
-		boolean[][] graph = { { false, true, false, true, false, false }, { false, false, true, false, false, false },
-				{ false, false, false, true, false, false }, { false, false, false, false, false, false },
-				{ false, false, false, true, false, true }, { false, false, true, false, false, false } };
+		boolean[][] graph = { { false, true, false, true, false, false }, 
+				{ false, false, true, false, false, false },
+				{ false, false, false, true, false, false },
+				{ false, false, false, false, false, false },
+				{ false, false, false, true, false, true },
+				{ false, false, true, false, false, false } 
+		};
 
 		topologicalSort(graph);
 	}
 
 	static Stack<Integer> st = new Stack<>();
+
 	public static void topologicalSort(boolean[][] graph) {
 
 		HashSet<Integer> visited = new HashSet<>();
 
 		for (int i = 0; i < graph.length; i++) {
-			if(visited.contains(i) == false) {
+			if (visited.contains(i) == false) {
 				topologicalSort(graph, visited, i);
 			}
 		}
-		
-		while(st.size() > 0) {
+
+		while (st.size() > 0) {
 			System.out.println(st.pop() + " ");
 		}
 
@@ -31,13 +36,13 @@ public class Topological_Sort {
 
 	private static void topologicalSort(boolean[][] graph, HashSet<Integer> visited, int root) {
 		visited.add(root);
-		
+
 		for (int i = 0; i < graph.length; i++) {
-			if(visited.contains(i) == false && graph[root][i] == true) {
+			if (visited.contains(i) == false && graph[root][i] == true) {
 				topologicalSort(graph, visited, i);
 			}
 		}
-		
+
 		st.push(root);
 	}
 }
