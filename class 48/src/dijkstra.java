fd11df91693cb35e28a3d793fd71c66763dbfd5a
MananBVP <manan200998@gmail.com>
2019-01-12 19:41:41 +0530

    floyddWarshall

--- class 48/src/dijkstra.java
+++ class 48/src/dijkstra.java
@@ -1,20 +1,19 @@
 import java.util.HashSet;
 import java.util.PriorityQueue;
 
-
 public class dijkstra {
 
 	public static void main(String[] args) {
-		
-		Integer[][] graph=new Integer[6][6];
-		graph[0][1]=2;
-		graph[0][2]=3;
-		graph[1][3]=5;
-		graph[2][4]=3;
-		graph[3][5]=2;
-		graph[4][5]=7;
-		graph[2][3]=1;
-		
+
+		Integer[][] graph = new Integer[6][6];
+		graph[0][1] = 2;
+		graph[0][2] = 3;
+		graph[1][3] = 5;
+		graph[2][4] = 3;
+		graph[3][5] = 2;
+		graph[4][5] = 7;
+		graph[2][3] = 1;
+
 		dijkstra(graph, 0);
 	}
 
@@ -31,19 +30,18 @@ public class dijkstra {
 
 		@Override
 		public int compareTo(DPair o) {
-			
+
 			return this.wsf - o.wsf;
 		}
 
 	}
 
 	public static void dijkstra(Integer[][] graph, int src) {
-		
-		
+
 		PriorityQueue<DPair> pq = new PriorityQueue<>();
 		HashSet<Integer> visited = new HashSet<>();
-		pq.add(new DPair(src,"" + src,0));
-		
+		pq.add(new DPair(src, "" + src, 0));
+
 		while (pq.size() > 0) {
 			// 1. remove
 			DPair rem = pq.remove();
@@ -59,10 +57,11 @@ public class dijkstra {
 
 			// 4.add*
 			for (int i = 0; i < graph.length; i++) {
-				if (visited.contains(i) == false && graph[rem.v][i] != null ) {
-					pq.add(new DPair(i,rem.psf + i, rem.wsf + graph[rem.v][i]));
+				if (visited.contains(i) == false && graph[rem.v][i] != null) {
+					pq.add(new DPair(i, rem.psf + i, rem.wsf + graph[rem.v][i]));
 				}
 			}
-	}
+		}
 
-	}}
+	}
+}
