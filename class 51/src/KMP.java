eee5554047758550d4bf02589466f4a07217d0b3
MananBVP <manan200998@gmail.com>
2019-01-29 15:36:44 +0530

    KMP

--- class 51/src/KMP.java
+++ class 51/src/KMP.java
@@ -2,55 +2,59 @@
 public class KMP {
 
 	public static void main(String[] args) {
-		String s = "aaabbbb";
 		String p = "aab";
+		String s = "aaabbbb";
+//		String p = "abc";
+//		String s = "jflkjsdjfsabckljflksjlkfabc";
 		solve(s, p);
 	}
 
 	public static void solve(String s, String p) {
-		int[] lps = preprocess(p);
-		int i = 0;
-		int j = 0;
-
+		int i = 0, j = 0;
+		int lps[] = preprocess(p);
 		while (i < s.length()) {
+
 			if (s.charAt(i) == p.charAt(j)) {
-				i++;
 				j++;
+				i++;
 				if (j == p.length()) {
 					System.out.println(p + " found at " + (i - j));
 					j = lps[j - 1];
 				}
+
 			} else {
-				if(j > p.length()) {
+				if (j > 0) {
 					j = lps[j - 1];
-				}else {
+				} else {
 					i++;
 				}
+
 			}
 		}
+
 	}
 
-	private static int[] preprocess(String p) {
+	public static int[] preprocess(String p) {
 		int[] lps = new int[p.length()];
-		int l = 0;
 		int i = 1;
-
+		int l = 0;
 		while (i < p.length()) {
-			if(p.charAt(l) == p.charAt(i)) {
+			if (p.charAt(i) == p.charAt(l)) {
 				l++;
 				lps[i] = l;
 				i++;
-			}else{
-				if(l > 0) {
-					l = lps[l -1];
-				}
-				else {
+			} else {
+				if (l > 0) {
+					l = lps[l - 1];
+				} else {
 					i++;
 				}
-			}
-		}
 
+			}
+
+		}
 		return lps;
+
 	}
 
 }
